19b0398d175c3bf072e17da6d61afc2789334e31
Emin-ahmedovf <eminoyun45@gmail.com>
2023-02-11 20:55:35 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,6 +4,6 @@ import os
 class Config():
     # Get these values from my.telegram.org
     # https://my.telegram.org
-    OWNER_ID = 5232474605
-    BOT_ID = 5307210494
+    OWNER_ID = 5273794514
+    BOT_ID = 5438309473
     LOG_GROUP_ID = -1001510168247
