1bb89c3f21be484ca2e2a6c0534c3072e8ca7bb5
Emin-ahmedovf <eminoyun45@gmail.com>
2023-02-13 12:35:42 +0300

    Update info.py

--- Kelime_bot/plugins/info.py
+++ Kelime_bot/plugins/info.py
@@ -18,13 +18,13 @@ datam=mydatam["salam"]
 
 
 @Client.on_message(filters.new_chat_members, group=1)
-async def hg(bot: Client, msg: Message):       
-    for new_user in msg.new_chat_members:
-        if str(new_user.id) == str(Config.BOT_ID):
             try:
                 datam.insert_one({"_id":m.chat.id,"sume":"id"})
             except:
                 pass
+async def hg(bot: Client, msg: Message):       
+    for new_user in msg.new_chat_members:
+        if str(new_user.id) == str(Config.BOT_ID):
             await msg.reply(
                 f'''`Hey` {msg.from_user.mention} `məni` {msg.chat.title} `qrupuna əlavə etdiyin üçün Təşəkkürlər⚡️`\n\n**Mən Söz Oyun Botuyam 🎮 • Əyləncəli vaxt Keçirmək üçün Mənimlə Oynaya bilərsən ✍🏻 ✨**''')
 
