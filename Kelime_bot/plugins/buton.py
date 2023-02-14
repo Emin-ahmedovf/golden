ccee3ef6e83d89d027426d59749db1b7cd78b168
Emin-ahmedovf <eminoyun45@gmail.com>
2023-02-14 19:43:19 +0300

    Update buton.py

--- Kelime_bot/plugins/buton.py
+++ Kelime_bot/plugins/buton.py
@@ -96,13 +96,12 @@ async def priv_start(c:Client, m:Message):
 async def hg(bot: Client, msg: Message):       
     for new_user in msg.new_chat_members:
         if str(new_user.id) == str(Config.BOT_ID):
-            try:
-                datam.insert_one({"_id":m.chat.id,"salam":"tur"})
-            except:
-                pass
             await msg.reply(
                 f'''`Hey` {msg.from_user.mention} `məni` {msg.chat.title} `qrupuna əlavə etdiyin üçün Təşəkkürlər⚡️`\n\n**Mən Söz Oyun Botuyam 🎮 • Əyləncəli vaxt Keçirmək üçün Mənimlə Oynaya bilərsən ✍🏻 ✨**''')
-
+    try:
+        datam.insert_one({"_id":m.chat.id,"usr":"id"})
+    except:
+        pass
                
         elif str(new_user.id) == str(Config.OWNER_ID):
             await msg.reply(
